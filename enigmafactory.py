5e9f4f87ababb680276fe387fbcffc2f846ab660
lorossi <contact@lorenzoros.si>
2022-01-01 22:36:54 +0100

    improved factory

--- enigmafactory.py
+++ enigmafactory.py
@@ -1,116 +1,124 @@
-
 from enigma import Enigma
 
+
 class EnigmaFactory:
+    def __init__(self) -> None:
+        self._settings = {
+            "Commercial": {
+                "rotors_map": {
+                    "IC": {"alphabet": "DMTWSILRUYQNKFEJCAZBPGXOHV", "notch": ["Z"]},
+                    "IIC": {"alphabet": "HQZGPJTMOBLNCIFDYAWVEUSRKX", "notch": ["Z"]},
+                    "IIIC": {"alphabet": "UQNTLSZFMREHDPXKIBVYGJCWOA", "notch": ["Z"]},
+                },
+                "ukw_map": {},
+                "etw_map": {},
+                "year": 1924,
+                "max_rotors": 3,
+            },
+            "Rocket": {
+                "rotors_map": {
+                    "I": {"alphabet": "JGDQOXUSCAMIFRVTPNEWKBLZYH", "notch": ["Z"]},
+                    "II": {"alphabet": "NTZPSFBOKMWRCJDIVLAEYUXHGQ", "notch": ["Z"]},
+                    "III": {"alphabet": "JVIUBHTCDYAKEQZPOSGXNRMWFL", "notch": ["Z"]},
+                },
+                "ukw_map": {"UKW": {"alphabet": "QYHOGNECVPUZTFDJAXWMKISRBL"}},
+                "etw_map": {"ETW": {"alphabet": "QWERTZUIOASDFGHJKPYXCVBNML"}},
+                "year": 1941,
+                "max_rotors": 3,
+            },
+            "Swiss": {
+                "rotors_map": {
+                    "I-K": {"alphabet": "PEZUOHXSCVFMTBGLRINQJWAYDK", "notch": ["Z"]},
+                    "II-K": {"alphabet": "ZOUESYDKFWPCIQXHMVBLGNJRAT", "notch": ["Z"]},
+                    "III-K": {"alphabet": "EHRVXGAOBQUSIMZFLYNWKTPDJC", "notch": ["Z"]},
+                },
+                "ukw_map": {"UKW-K": {"alphabet": "IMETCGFRAYSQBZXWLHKDVUPOJN"}},
+                "etw_map": {"ETW-K": {"alphabet": "QWERTZUIOASDFGHJKPYXCVBNML"}},
+                "year": 1939,
+                "max_rotors": 3,
+            },
+            "M3": {
+                "rotors_map": {
+                    "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
+                    "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
+                    "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
+                },
+                "ukw_map": {
+                    "A": {"alphabet": "EJMZALYXVBWFCRQUONTSPIKHGD"},
+                    "B": {"alphabet": "YRUHQSLDPXNGOKMIEBFZCWVJAT"},
+                    "C": {"alphabet": "FVPJIAOYEDRZXWGCTKUQSBNMHL"},
+                },
+                "etw_map": {},
+                "year": 1938,
+                "max_rotors": 3,
+            },
+            "M4": {
+                "rotors_map": {
+                    "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
+                    "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
+                    "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
+                    "IV": {"alphabet": "ESOVPZJAYQUIRHXLNFTGKDCMWB", "notch": ["J"]},
+                    "V": {"alphabet": "VZBRGITYUPSDNHLXAWMJQOFECK", "notch": ["Z"]},
+                    "VI": {
+                        "alphabet": "JPGVOUMFYQBENHZRDKASXLICTW",
+                        "notch": ["Z", "M"],
+                    },
+                    "VII": {
+                        "alphabet": "NZJHGRCXMYSWBOUFAIVLPEKQDT",
+                        "notch": ["Z", "M"],
+                    },
+                    "VIII": {
+                        "alphabet": "FKQHTLXOCBJSPDZRAMEWNIUYGV",
+                        "notch": ["Z", "M"],
+                    },
+                },
+                "etw_map": {},
+                "year": 1938,
+                "max_rotors": 4,
+            },
+            "M4-Thin": {
+                "rotors_map": {
+                    "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
+                    "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
+                    "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
+                    "IV": {"alphabet": "ESOVPZJAYQUIRHXLNFTGKDCMWB", "notch": ["J"]},
+                    "V": {"alphabet": "VZBRGITYUPSDNHLXAWMJQOFECK", "notch": ["Z"]},
+                    "VI": {
+                        "alphabet": "JPGVOUMFYQBENHZRDKASXLICTW",
+                        "notch": ["Z", "M"],
+                    },
+                    "VII": {
+                        "alphabet": "NZJHGRCXMYSWBOUFAIVLPEKQDT",
+                        "notch": ["Z", "M"],
+                    },
+                    "VIII": {
+                        "alphabet": "FKQHTLXOCBJSPDZRAMEWNIUYGV",
+                        "notch": ["Z", "M"],
+                    },
+                },
+                "ukw_map": {
+                    "A-Thin": {"alphabet": "ENKQAUYWJICOPBLMDXZVFTHRGS"},
+                    "B-Thin": {"alphabet": "RDOBJNTKVEHMLFCWZAXGYIPSUQ"},
+                },
+                "etw_map": {},
+                "year": 1939,
+                "max_rotors": 4,
+            },
+        }
+
     def createEnigma(self, model: str = "M3") -> Enigma:
-        kwargs = {}
+        if self._settings.get(model):
+            # add model name to settings
+            self._settings[model]["model"] = model
+            return Enigma(**self._settings[model])
 
-        match model:
-            case "Commercial":
-                kwargs["rotors_map"] = {
-                        "IC": {"alphabet": "DMTWSILRUYQNKFEJCAZBPGXOHV", "notch": ["Z"]},
-                        "IIC": {"alphabet": "HQZGPJTMOBLNCIFDYAWVEUSRKX", "notch": ["Z"]},
-                        "IIIC": {"alphabet": "UQNTLSZFMREHDPXKIBVYGJCWOA", "notch": ["Z"]},
-                }
-                kwargs["ukw_map"] = {}
-                kwargs["etw_map"] = {}
-                kwargs["year"] = 1924
-                kwargs["max_rotors"] = 3
-
-            case "Rocket":
-                kwargs["rotors_map"] = {
-                        "I": {"alphabet": "JGDQOXUSCAMIFRVTPNEWKBLZYH", "notch": ["Z"]},
-                        "II": {"alphabet": "NTZPSFBOKMWRCJDIVLAEYUXHGQ", "notch": ["Z"]},
-                        "III": {"alphabet": "JVIUBHTCDYAKEQZPOSGXNRMWFL", "notch": ["Z"]},
-                }
-                kwargs["ukw_map"] = {
-                    "UKW": {"alphabet": "QYHOGNECVPUZTFDJAXWMKISRBL"},
-                }
-                kwargs["etw_map"] = {
-                    "ETW": {"alphabet": "QWERTZUIOASDFGHJKPYXCVBNML"},
-                }
-                kwargs["year"] = 1941
-                kwargs["max_rotors"] = 3
-
-            case "Swiss":
-                    kwargs["rotors_map"] = {
-                        "I-K": {"alphabet": "PEZUOHXSCVFMTBGLRINQJWAYDK", "notch": ["Z"]},
-                        "II-K": {"alphabet": "ZOUESYDKFWPCIQXHMVBLGNJRAT", "notch": ["Z"]},
-                        "III-K": {"alphabet": "EHRVXGAOBQUSIMZFLYNWKTPDJC", "notch": ["Z"]},
-                    }
-                    kwargs["ukw_map"] = {
-                        "UKW-K": {"alphabet": "IMETCGFRAYSQBZXWLHKDVUPOJN"},
-                    }
-                    kwargs["etw_map"] = {
-                        "ETW-K": {"alphabet": "QWERTZUIOASDFGHJKPYXCVBNML"},
-                    }
-                    kwargs["year"] = 1939
-                    kwargs["max_rotors"] = 3
-
-            case "M3":
-                    kwargs["rotors_map"] = {
-                        "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
-                        "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
-                        "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
-                    }
-                    kwargs["ukw_map"] = {
-                        "A": {"alphabet": "EJMZALYXVBWFCRQUONTSPIKHGD"},
-                        "B": {"alphabet": "YRUHQSLDPXNGOKMIEBFZCWVJAT"},
-                        "C": {"alphabet": "FVPJIAOYEDRZXWGCTKUQSBNMHL"},
-                    }
-                    kwargs["etw_map"] = {}
-                    kwargs["year"] = 1938
-                    kwargs["max_rotors"] = 3
-
-            case "M4":
-                    kwargs["rotors_map"] = {
-                        "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
-                        "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
-                        "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
-                        "IV": {"alphabet": "ESOVPZJAYQUIRHXLNFTGKDCMWB", "notch": ["J"]},
-                        "V": {"alphabet": "VZBRGITYUPSDNHLXAWMJQOFECK", "notch": ["Z"]},
-                        "VI": {"alphabet": "JPGVOUMFYQBENHZRDKASXLICTW", "notch": ["Z", "M"]},
-                        "VII": {"alphabet": "NZJHGRCXMYSWBOUFAIVLPEKQDT", "notch": ["Z", "M"]},
-                        "VIII": {"alphabet": "FKQHTLXOCBJSPDZRAMEWNIUYGV", "notch": ["Z", "M"]},
-                    }
-                    kwargs["ukw_map"] = {
-                        "A": {"alphabet": "EJMZALYXVBWFCRQUONTSPIKHGD"},
-                        "B": {"alphabet": "YRUHQSLDPXNGOKMIEBFZCWVJAT"},
-                        "C": {"alphabet": "FVPJIAOYEDRZXWGCTKUQSBNMHL"},
-                    }
-                    kwargs["etw_map"] = {}
-                    kwargs["year"] = 1938
-                    kwargs["max_rotors"] = 4
-
-            case "M4-Thin":
-                    kwargs["rotors_map"] = {
-                        "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
-                        "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
-                        "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
-                        "IV": {"alphabet": "ESOVPZJAYQUIRHXLNFTGKDCMWB", "notch": ["J"]},
-                        "V": {"alphabet": "VZBRGITYUPSDNHLXAWMJQOFECK", "notch": ["Z"]},
-                        "VI": {"alphabet": "JPGVOUMFYQBENHZRDKASXLICTW", "notch": ["Z", "M"]},
-                        "VII": {"alphabet": "NZJHGRCXMYSWBOUFAIVLPEKQDT", "notch": ["Z", "M"]},
-                        "VIII": {"alphabet": "FKQHTLXOCBJSPDZRAMEWNIUYGV", "notch": ["Z", "M"]},
-                    }
-                    kwargs["ukw_map"] = {
-                        "A-Thin": {"alphabet": "ENKQAUYWJICOPBLMDXZVFTHRGS"},
-                        "B-Thin": {"alphabet": "RDOBJNTKVEHMLFCWZAXGYIPSUQ"},
-                    }
-                    kwargs["etw_map"] = {}
-                    kwargs["year"] = 1939
-                    kwargs["max_rotors"] = 4
-
-            case _:
-                raise ValueError(f"Invalid model. Valid models are: {', '.join(self.available_models)}")
-
-
-        kwargs["model"] = model
-        return Enigma(**kwargs)
+        raise ValueError(
+            f"Invalid model. Valid models are: {', '.join(self.available_models)}"
+        )
 
     def __str__(self) -> str:
         return f"Class used to create historically accurate Enigma machines. Available models: {', '.join(self.available_models)}"
 
     @property
     def available_models(self) -> list[str]:
-        return ["Commercial", "Rocket", "Swiss", "M3", "M4", "M4-Thin"]
+        return [m for m in self._settings]
