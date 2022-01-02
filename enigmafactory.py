f03e29e8a29ee20f025865bb22870b9f887354fa
lorossi <contact@lorenzoros.si>
2022-01-02 17:48:24 +0100

    moved settings to own file

--- enigmafactory.py
+++ enigmafactory.py
@@ -1,116 +1,13 @@
 from enigma import Enigma
+import ujson
 
 
 class EnigmaFactory:
     def __init__(self) -> None:
-        # check this list for more settings https://www.cryptomuseum.com/crypto/enigma/wiring.htm
-        self._settings = {
-            "Commercial": {
-                "rotors_map": {
-                    "IC": {"alphabet": "DMTWSILRUYQNKFEJCAZBPGXOHV", "notch": ["Z"]},
-                    "IIC": {"alphabet": "HQZGPJTMOBLNCIFDYAWVEUSRKX", "notch": ["Z"]},
-                    "IIIC": {"alphabet": "UQNTLSZFMREHDPXKIBVYGJCWOA", "notch": ["Z"]},
-                },
-                "ukw_map": {},
-                "etw_map": {},
-                "year": 1924,
-                "max_rotors": 3,
-            },
-            "Rocket": {
-                "rotors_map": {
-                    "I": {"alphabet": "JGDQOXUSCAMIFRVTPNEWKBLZYH", "notch": ["Z"]},
-                    "II": {"alphabet": "NTZPSFBOKMWRCJDIVLAEYUXHGQ", "notch": ["Z"]},
-                    "III": {"alphabet": "JVIUBHTCDYAKEQZPOSGXNRMWFL", "notch": ["Z"]},
-                },
-                "ukw_map": {"UKW": {"alphabet": "QYHOGNECVPUZTFDJAXWMKISRBL"}},
-                "etw_map": {"ETW": {"alphabet": "QWERTZUIOASDFGHJKPYXCVBNML"}},
-                "year": 1941,
-                "max_rotors": 3,
-            },
-            "Swiss": {
-                "rotors_map": {
-                    "I-K": {"alphabet": "PEZUOHXSCVFMTBGLRINQJWAYDK", "notch": ["Z"]},
-                    "II-K": {"alphabet": "ZOUESYDKFWPCIQXHMVBLGNJRAT", "notch": ["Z"]},
-                    "III-K": {"alphabet": "EHRVXGAOBQUSIMZFLYNWKTPDJC", "notch": ["Z"]},
-                },
-                "ukw_map": {"UKW-K": {"alphabet": "IMETCGFRAYSQBZXWLHKDVUPOJN"}},
-                "etw_map": {"ETW-K": {"alphabet": "QWERTZUIOASDFGHJKPYXCVBNML"}},
-                "year": 1939,
-                "max_rotors": 3,
-            },
-            "M3": {
-                "rotors_map": {
-                    "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
-                    "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
-                    "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
-                },
-                "ukw_map": {
-                    "A": {"alphabet": "EJMZALYXVBWFCRQUONTSPIKHGD"},
-                    "B": {"alphabet": "YRUHQSLDPXNGOKMIEBFZCWVJAT"},
-                    "C": {"alphabet": "FVPJIAOYEDRZXWGCTKUQSBNMHL"},
-                },
-                "etw_map": {},
-                "year": 1938,
-                "max_rotors": 3,
-            },
-            "M4": {
-                "rotors_map": {
-                    "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
-                    "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
-                    "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
-                    "IV": {"alphabet": "ESOVPZJAYQUIRHXLNFTGKDCMWB", "notch": ["J"]},
-                    "V": {"alphabet": "VZBRGITYUPSDNHLXAWMJQOFECK", "notch": ["Z"]},
-                    "VI": {
-                        "alphabet": "JPGVOUMFYQBENHZRDKASXLICTW",
-                        "notch": ["Z", "M"],
-                    },
-                    "VII": {
-                        "alphabet": "NZJHGRCXMYSWBOUFAIVLPEKQDT",
-                        "notch": ["Z", "M"],
-                    },
-                    "VIII": {
-                        "alphabet": "FKQHTLXOCBJSPDZRAMEWNIUYGV",
-                        "notch": ["Z", "M"],
-                    },
-                },
-                "ukw_map": {
-                    "A": {"alphabet": "EJMZALYXVBWFCRQUONTSPIKHGD"},
-                    "B": {"alphabet": "YRUHQSLDPXNGOKMIEBFZCWVJAT"},
-                    "C": {"alphabet": "FVPJIAOYEDRZXWGCTKUQSBNMHL"},
-                },
-                "etw_map": {},
-                "year": 1938,
-                "max_rotors": 4,
-            },
-            "M4-Thin": {
-                "rotors_map": {
-                    "I": {"alphabet": "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "notch": ["Q"]},
-                    "II": {"alphabet": "AJDKSIRUXBLHWTMCQGZNPYFVOE", "notch": ["E"]},
-                    "III": {"alphabet": "BDFHJLCPRTXVZNYEIWGAKMUSQO", "notch": ["V"]},
-                    "IV": {"alphabet": "ESOVPZJAYQUIRHXLNFTGKDCMWB", "notch": ["J"]},
-                    "V": {"alphabet": "VZBRGITYUPSDNHLXAWMJQOFECK", "notch": ["Z"]},
-                    "VI": {
-                        "alphabet": "JPGVOUMFYQBENHZRDKASXLICTW",
-                        "notch": ["Z", "M"],
-                    },
-                    "VII": {
-                        "alphabet": "NZJHGRCXMYSWBOUFAIVLPEKQDT",
-                        "notch": ["Z", "M"],
-                    },
-                    "VIII": {
-                        "alphabet": "FKQHTLXOCBJSPDZRAMEWNIUYGV",
-                        "notch": ["Z", "M"],
-                    },
-                },
-                "ukw_map": {
-                    "A-Thin": {"alphabet": "ENKQAUYWJICOPBLMDXZVFTHRGS"},
-                    "B-Thin": {"alphabet": "RDOBJNTKVEHMLFCWZAXGYIPSUQ"},
-                },
-                "etw_map": {},
-                "year": 1939,
-                "max_rotors": 4,
-            },
-        }
+        # settings sourced from https://www.cryptomuseum.com/crypto/enigma/wiring.htm
+        # TODO remember to credit it in readme
+        with open("settings.json") as f:
+            self._settings = ujson.load(f)
 
     def __str__(self) -> str:
         return f"Class used to create historically accurate Enigma machines. Available models: {', '.join(self.available_models)}"
